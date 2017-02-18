3b7b1ead2c5aba9899616f493bd92ce615a9bcf6
falk <falk.schwiefert@gmail.com>
2017-02-18 13:42:23 -0800

    fucking with heroku

--- index.js
+++ index.js
@@ -16,40 +16,12 @@ console.log("websocket server created")
 
 wss.on("connection", function(ws) {
 
-  console.log('message received', msgString)
+  console.log('websocket connected')
+
+
 
   // ws.on("close", function() {
   //   console.log("websocket connection close")
   //   clearInterval(id)
   // })
 })
-
-
-
-// var express = require('express')
-// var app = express()
-//
-// var expressWs = require('express-ws')(app)
-//
-// var nextId = 1
-// var clients = {}
-//
-// app.ws('/chat', function(ws, req) {
-//   var clientId = nextId
-//   clients[clientId] = {ws : ws}
-//   nextId++
-//   ws.on('message', function(msgString) {
-//     console.log('message received', msgString)
-//     var msg = JSON.parse(msgString)
-//     var msgToSend = JSON.stringify({
-//       clientId: clientId,
-//       message: msg.messageText
-//     })
-//     Object.keys(clients).forEach(function(client) {
-//       console.log('sending message from server', msgToSend)
-//       clients[client].ws.send(msgToSend)
-//     })
-//   })
-// })
-//
-// app.use(express.static('public'))
